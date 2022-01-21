ff5df7d2e93995691894f7c7d5fabae503c39af6
abel-abel-1234 <97020402+abel-abel-1234@users.noreply.github.com>
2022-01-21 11:35:27 +0530

    Update help.js

--- commands/help.js/help.js
+++ commands/help.js/help.js
@@ -1,1 +1,31 @@
+const Discord = require('discord.js');
+const fs = require("fs");
+module.exports.run = async (client, message, args) => {
+    fs.readdir('./commands/', (err, files) => {
+        if(err) console.error(err);
 
+        let jsfiles = files.filter(f => f.split(".").pop() === "js");
+        if(jsfiles.length <= 0) {
+            console.log("No commands to load!");
+            return;
+        }
+        
+        let result = jsfiles.map(f => `${require(`./${f}`).config.name} \n \`${require(`./${f}`).config.description}\` `).join('\n');
+        message.channel.send(new Discord.MessageEmbed()
+	
+			.setTitle(`All Available Commands`)
+			.addField('Made By', `\`Hecker.#6279\``, true)
+			.addField('Prefix', `\`${process.env.prefix}\``, true)
+            .addField('invite', `[click me](https://discord.com/api/oauth2/authorize?client_id=933581594042724362&permissions=8&scope=applications.commands%20bot)`, true)
+                        .setDescription(result)
+			.setColor('RANDOM'),
+		);
+})
+}
+
+
+module.exports.config = {
+	name: 'help',
+	description: 'shows all available commands',
+	aliases: [],
+};
